107a749e78136dfb086145925441d3b3e630ecbd
Michael Vandendriessche <michael.vandendriessche@ordina.be>
2017-02-08 16:18:10 +0100

    improved search

--- src/app/shared/search/search.component.ts
+++ src/app/shared/search/search.component.ts
@@ -1,26 +1,43 @@
 import {Component, OnInit, ViewEncapsulation} from "@angular/core";
 import {SearchService} from "../../providers/search.service";
-import {Observable} from "rxjs";
+import {FormControl} from "@angular/forms";
+
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'search-employees',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 
 export class SearchComponent implements OnInit {
 
-  employee = Observable;
+  searchTerm = new FormControl();
+  searchResult: any[] = [];
 
   constructor(private searchService: SearchService) {
   }
 
   ngOnInit() {
-    this.searchService.searchEmployees().subscribe(res => {
-      this.employee = res;
-      console.log(res, "res")
-    })
+    this.searchTerm.valueChanges
+      .debounceTime(400)
+      .distinctUntilChanged()
+      .do((val)=>{
+         if(! (val.length > 0)){
+           this.clearSearchResults();
+         }
+        })
+      .filter(val => val.length > 0)
+      .do(() => this.clearSearchResults())
+      .switchMap(searchTerm => this.searchService.searchEmployee(searchTerm))
+      .subscribe(searchOutput => {
+        this.searchResult.push(searchOutput);
+      });
   }
 
+  private clearSearchResults() {
+    this.searchResult = []
+  }
 }
