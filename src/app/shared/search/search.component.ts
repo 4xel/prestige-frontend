9327e58b6a770460d87187f71454a14f4dfb3db7
Michael Vandendriessche <michael.vandendriessche@ordina.be>
2017-02-08 16:21:51 +0100

    formatting

--- src/app/shared/search/search.component.ts
+++ src/app/shared/search/search.component.ts
@@ -1,9 +1,8 @@
 import {Component, OnInit, ViewEncapsulation} from "@angular/core";
 import {SearchService} from "../../providers/search.service";
 import {FormControl} from "@angular/forms";
-
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/switchMap';
+import "rxjs/add/operator/debounceTime";
+import "rxjs/add/operator/switchMap";
 
 @Component({
   selector: 'search-employees',
@@ -20,22 +19,18 @@ export class SearchComponent implements OnInit {
   constructor(private searchService: SearchService) {
   }
 
-  ngOnInit() {
-    this.searchTerm.valueChanges
-      .debounceTime(400)
-      .distinctUntilChanged()
-      .do((val)=>{
-         if(! (val.length > 0)){
-           this.clearSearchResults();
-         }
-        })
-      .filter(val => val.length > 0)
-      .do(() => this.clearSearchResults())
-      .switchMap(searchTerm => this.searchService.searchEmployee(searchTerm))
-      .subscribe(searchOutput => {
-        this.searchResult.push(searchOutput);
-      });
-  }
+    ngOnInit() {
+        this.searchTerm.valueChanges
+            .debounceTime(400)
+            .distinctUntilChanged()
+            .do((val) => { if (!(val.length > 0)) {this.clearSearchResults();} })
+            .filter(val => val.length > 0)
+            .do(() => this.clearSearchResults())
+            .switchMap(searchTerm => this.searchService.searchEmployee(searchTerm))
+            .subscribe(searchOutput => {
+                this.searchResult.push(searchOutput);
+            });
+    }
 
   private clearSearchResults() {
     this.searchResult = []
