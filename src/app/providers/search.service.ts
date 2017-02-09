e81386b38b28d7531cf35d2029cb2d486e938686
Michael Vandendriessche <michael.vandendriessche@ordina.be>
2017-02-09 17:05:28 +0100

    re-added searchEmployee in SearchService

--- src/app/providers/search.service.ts
+++ src/app/providers/search.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {AngularFire} from  'angularfire2';
+
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/observable/fromPromise';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/merge';
+
+import {Observable} from "rxjs";
 
 @Injectable()
 export class SearchService {
 
-  constructor() { }
+  employees: any;
+
+  constructor(private af: AngularFire) {
+    this.employees = this.af.database.object('/employees')
+      .map(result => result);
+  }
+
+  public searchEmployee(searchTerm: any) {
+    return this.employees
+      .flatMap(Observable.from)
+      .filter((employee) => employee.name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1)
+  }
+
+  public searchEmployees() {
+    return this.af.database.list('/employees')
+  }
 
 }
